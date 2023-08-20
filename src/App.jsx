599f3870f8e8ca11f2e187462f82cf5cf7132bda
Paris Pettis <pettis_paris@hotmail.com>
2023-08-20 12:24:14 +0300

    local storage update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { NewTodoForm } from './NewTodoForm'
 import { TodoList } from './TodoList'
 
 import './App.scss'
 
 export default function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
 
   function addTodo(title) {
     const id = crypto.randomUUID()
